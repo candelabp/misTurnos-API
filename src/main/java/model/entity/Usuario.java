99459294e90d51f78ca73f2d07f3496e5fc36f3f
candelabp <candepuerta@gmail.com>
2026-01-08 20:10:58 -0300

    Initial commit

---
+++ src/main/java/model/entity/Usuario.java
@@ -0,0 +1,27 @@
+package model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+@Entity
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    private String nombre;
+    private String email;
+    private Rol rol;
+    private boolean activo;
+
+}
---
+++ src/test/java/com/example/sistematurnos/SistematurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistematurnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistematurnosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
